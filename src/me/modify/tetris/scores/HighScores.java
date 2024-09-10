a2b488820e3e0298a41aa095ef55ca037a827299
JModify <joshua.lavagna.slater@gmail.com>
2024-09-10 12:56:17 +1000

    [+] Added Score object which holds a score entry and is comparable for sorting later. [+] Added HighScores which will hold all high scores for the game - eventually functionality for saving/loading to a text file will be added.

---
+++ src/me/modify/tetris/scores/HighScores.java
@@ -0,0 +1,27 @@
+package me.modify.tetris.scores;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class HighScores {
+
+    private Set<Score> scores;
+
+    public HighScores(Set<Score> scores) {
+        this.scores = scores;
+    }
+
+    private List<Score> getSortedList() {
+        return scores.stream().sorted().toList();
+    }
+
+    public void addScore(Score score) {
+        scores.add(score);
+    }
+
+    public List<Score> getTopScores() {
+        return getSortedList().stream().limit(10).toList();
+    }
+
+
+}
---
+++ src/me/modify/tetris/scores/Score.java
@@ -0,0 +1,33 @@
+package me.modify.tetris.scores;
+
+public class Score implements Comparable<Score> {
+
+    private String name;
+    private int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Score o) {
+        return Integer.compare(this.score, o.getScore());
+    }
+}
